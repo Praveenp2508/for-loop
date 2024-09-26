41d479b84a0b8343cd2e890d21141f12c8c0ab9c
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-09-26 10:06:07 +0530

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,19 @@
+# for loop----
+
+for j in range(0,11):
+    print(j)
+
+for i in range(2,22,2):
+    print(i)
+
+
+for index in[1,2,3,4]:
+    print(index)
+
+for a in[1,2,3,4,5,6,7,8,9,10]:
+    print('6','x',a,'=',a*6)
+
+i=int(input('enter the number='))
+for a in[1,2,3,4,5,6,7,8,9,10]:
+    print(i,'x',a,'=',a*i)
+
